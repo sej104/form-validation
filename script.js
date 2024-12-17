3e809331ffb6b1fc3e1f6f0d131f42e0ce9c701c
Sergio Joya <sejoya104@gmail.com>
2024-12-17 14:09:15 -0500

    Add validation for zip codes

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ confirmPassword.addEventListener("input", () => {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   resetErrors();
+  if (country.value) checkZipCode();
+
   for (const field of fields) {
     if (!field.validity.valid) {
       field.reportValidity();
@@ -40,3 +42,32 @@ form.addEventListener("submit", (e) => {
 
   form.reset();
 });
+
+function checkZipCode() {
+  const constraints = {
+    ch: [
+      "^(CH-)?\\d{4}$",
+      "Swiss postal codes must have exactly 4 digits: e.g. CH-1950 or 1950",
+    ],
+    fr: [
+      "^(F-)?\\d{5}$",
+      "French postal codes must have exactly 5 digits: e.g. F-75012 or 75012",
+    ],
+    de: [
+      "^(D-)?\\d{5}$",
+      "German postal codes must have exactly 5 digits: e.g. D-12345 or 12345",
+    ],
+    nl: [
+      "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+      "Dutch postal codes must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+    ],
+  };
+
+  const constraint = new RegExp(constraints[country.value][0], "");
+
+  if (constraint.test(zipCode.value)) {
+    zipCode.setCustomValidity("");
+  } else {
+    zipCode.setCustomValidity(constraints[country.value][1]);
+  }
+}
