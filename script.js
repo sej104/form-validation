f7c8bcd5a2c10fde66b6d6fcc976ea7414e184c4
Sergio Joya <sejoya104@gmail.com>
2024-12-17 10:23:00 -0500

    Add validation on form submit

---
+++ script.js
@@ -0,0 +1,31 @@
+const email = document.querySelector("#email");
+const country = document.querySelector("#country");
+const zipCode = document.querySelector("#zip-code");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const form = document.querySelector("form");
+const fields = [email, country, zipCode, password, confirmPassword];
+
+const resetErrors = () => {
+  fields.forEach((field) => field.setCustomValidity(""));
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  resetErrors();
+  for (const field of fields) {
+    if (!field.validity.valid) {
+      field.reportValidity();
+      return;
+    }
+  }
+
+  if (password.value !== confirmPassword.value) {
+    password.setCustomValidity("Passwords do not match");
+    confirmPassword.setCustomValidity("Passwords do not match");
+    confirmPassword.reportValidity();
+    return;
+  }
+
+  form.reset();
+});
